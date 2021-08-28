ad48dc690490f7cede2df32eb94faec1d1228299
gideonvw <gideon.vanwallenberg@kpn.com>
2021-08-28 18:07:01 +0200

    originele versie weer

--- MyPyRepository1.py
+++ MyPyRepository1.py
@@ -1,6 +1,7 @@
 # Dit is de Hello World van mijn nieuwe repository
 # Dit is update 2 op zaterdagochtend op de github.com
 def afdrukken(  sTxt ):	// druk een tekst af
+    // Oke nu
     print( sTxt )
 
 afdrukken ("Hello world")
