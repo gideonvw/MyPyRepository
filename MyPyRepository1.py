3d0f9dea3b9327303947abf33e4bac67f2872a40
gideonvw <gideon.vanwallenberg@kpn.com>
2021-08-28 14:41:49 +0200

    Nieuw

--- MyPyRepository1.py
+++ MyPyRepository1.py
@@ -1,3 +1,6 @@
 # Dit is de Hello World van mijn nieuwe repository
 # Dit is update 2 op zaterdagochtend op de github.com
-print ("Hello world")
+def afdrukken(  sTxt ):
+    print( sTxt )
+
+afdrukken ("Hello world")
